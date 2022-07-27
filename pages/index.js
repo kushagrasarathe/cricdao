e79f4fdd99a003379268fa2bb2f42771edaae7ee
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-07-27 14:33:58 +0530

    added roadmap

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,9 @@ import logo from "../src/assets/logo.svg";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={styles.contsainer}>
       <div className={styles.hero_section}>
+     
         <div className={styles.hero_content}>
           <h1>CricDAO is a one-stop platform for cricket lovers</h1>
           <p>
@@ -24,6 +25,9 @@ export default function Home() {
         <div className={styles.hero_image}>
           <Image src={hero} />
         </div>
+        {/* <div className={styles.roadmap}>
+          <Image className={styles.roadmap} src={roadmap} />
+        </div> */}
       </div>
 
       <div className={styles.features_section}>
@@ -35,84 +39,100 @@ export default function Home() {
           <li>You can become DAO member by purchasing a DAO NFT.</li>
           <li>You can buy Player NFTs , trade and play games with them.</li>
           <li>You can also predict match result and win rewards</li>
-        
         </div>
         <button className={styles.redirect_button}>Join CricDAO</button>
       </div>
 
-      <div className={styles.redirect_section}>
-        <button className={styles.redirect_button}>Battles</button>
-        <button className={styles.redirect_button}>Join Whitelist</button>
-      </div>
-
-      <main className={styles.main}>
-        {/* <div className={styles.hero}> */}
-        {/* </div> */}
-        <div className={styles.content}>
-          <h1>Welcome to CricDAO</h1>
-          <p>
-            CricDAO lets you collect your favourite cricket players as NFT cards
-            and participate in battles, where you can predict match moments and
-            win rewards.
-          </p>
-          <p>
-            You can also trade your player NFTs in the marketplace and buy new
-            Player NFTs.
-          </p>
-        </div>
-      </main>
-
-      <div className={styles.whitelist}>
-        <div className={styles.content}>
-          <button className={styles.whitelist_btn}>Join Whitelist</button>
-        </div>
-        <div className={styles.stumps}>
-          <Image src={stumps} />
-        </div>
-      </div>
-
-      {/* mint section */}
-
-      <div className={styles.nft_section}>
-        <div className={styles.nft}>
-          <Image className={styles.nft} src={nft} />
-        </div>
-        <div className={styles.content}>
-          <h1>Mint NFT</h1>
-          <p>Mint NFT and Get Access to:</p>
-
-          <ul>
+      <div className={styles.mint_heading}>
+        <h1>Mint NFT</h1>
+        <div className={styles.mint_section}>
+          <div>
+            <h2>Mint NFT and Get Access to:</h2>
             <li>Whitelisting</li>
             <li>NFT Marketplace</li>
             <li>Battles</li>
             <li>Token</li>
             <li>And more future perks</li>
-          </ul>
-          <button className={styles.whitelist_btn}>Mint NFT</button>
+            <button className={styles.redirect_button}>Mint NFT</button>
+          </div>
+          <div className={styles.nft}>
+            <Image className={styles.nft} src={nft} />
+          </div>
         </div>
       </div>
 
-      <div className={styles.roadmap_section}>
-        <div className={styles.content}>
-          {/* <h1>Roadmap</h1> */}
-          {/* <ul>
-            <li>Whitelisting</li>
-            <li>DAO NFT launch</li>
-            <li>Match prediction</li>
-            <li>Token launch</li>
-            <li>NFT Utility game</li>
-            <li>Buy Cricket match tickets online in form of cool NFTs</li>
-            <li>
-              Get players Onboard to the platform, they can be rewarded for
-              their contribution
-            </li>
-          </ul> */}
-          {/* <button className={styles.whitelist_btn}>Mint NFT</button> */}
+<div className={styles.roadmap_section}>
+
+      <h1>Roadmap</h1>
+      <div className={styles.timeline}>
+
+        <div className={`${styles.container} ${styles.left}`}>
+          <div className={styles.content}>
+            {/* <h2>1</h2> */}
+            <p>
+              Whitelisting
+            </p>
+          </div>
         </div>
-        <div className={styles.roadmap}>
-          <Image className={styles.roadmap} src={roadmap} />
+        <div className={`${styles.container} ${styles.right}`}>
+          <div className={styles.content}>
+            {/* <h2>2</h2> */}
+            <p>
+             DAO NFT Launch
+            </p>
+          </div>
+        </div>
+        <div className={`${styles.container} ${styles.left}`}>
+          <div className={styles.content}>
+            {/* <h2>3</h2> */}
+            <p>
+              Match Prediction
+            </p>
+          </div>
+        </div>
+        <div className={`${styles.container} ${styles.right}`}>
+          <div className={styles.content}>
+            {/* <h2>4</h2> */}
+            <p>
+             Token Launch
+            </p>
+          </div>
+        </div>
+        <div className={`${styles.container} ${styles.left}`}>
+          <div className={styles.content}>
+            {/* <h2>5</h2> */}
+            <p>
+              NFT Utility Game
+            </p>
+          </div>
+        </div>
+        <div className={`${styles.container} ${styles.right}`}>
+          <div className={styles.content}>
+            {/* <h2>6</h2> */}
+            <p>
+              Match Tickets
+            </p>
+          </div>
+        </div>
+        <div className={`${styles.container} ${styles.left}`}>
+          <div className={styles.content}>
+            {/* <h2>7</h2> */}
+            <p>
+              User Onboarding
+            </p>
+          </div>
         </div>
       </div>
+
+      </div>
+
+     
+
+      
+
+
+
+
     </div>
   );
 }
